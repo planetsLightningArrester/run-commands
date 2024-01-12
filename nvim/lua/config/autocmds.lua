5b5485611e56ad396bf95872791b269c36e51d31
Planets Lightning Arrester <franciscogssm94@gmail.com>
2024-01-12 02:44:21 -0300

    refactor: remove wrap for markdown (it's the default)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,11 +3,3 @@
 -- Add any additional autocmds here
 require("config.dap.autocmds")
 require("config.perforce.autocmds")
-
--- Wrap lines in Markdown files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.md" },
-  callback = function(_)
-    vim.api.nvim_exec2("setlocal textwidth=80")
-  end,
-})
