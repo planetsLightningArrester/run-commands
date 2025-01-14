a98e738c0b3cfc8af2bd6af6ee3f6c3119903458
Planets Lightning Arrester <franciscogssm94@gmail.com>
2025-01-14 01:54:52 -0300

    feat(nvim): update configs to support the latest nvim version

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.common")
-require("config.telescope")
 require("config.colorscheme")
 require("config.python")
 require("config.shfmt")
--- nvim/lua/config/telescope.lua
+++
@@ -1,9 +0,0 @@
-require("telescope").setup {
-  pickers = {
-    live_grep = {
-      additional_args = function(_)
-        return { "--hidden" }
-      end
-    },
-  },
-}
